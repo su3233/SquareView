20834250da2c0139d71aba9361ffd37030e1f126
sts511 <33302991+su3233@users.noreply.github.com>
2021-03-02 16:05:24 +0800

    1\add MyLog method

--- squareViewLib/src/main/java/com/example/squareviewlib/MyLog.java
+++ squareViewLib/src/main/java/com/example/squareviewlib/MyLog.java
@@ -1,15 +1,34 @@
 package com.example.squareviewlib;
 
+import android.util.Log;
+
 /**
  * @author: SuTs
  * @created at: 2021/3/2 15:37
  * @Describe:Log日志打印
  */
 public class MyLog {
+    private static final String TAG = "MyLog";
 
+    public static void e(String msg) {
+        if (isLog()) {
+            Log.e(TAG, "e: " + msg);
+        }
+    }
 
+    public static void w(String msg) {
+        if (isLog()) {
+            Log.w(TAG, "w: " + msg);
+        }
+    }
 
-    boolean isLog() {
+    public static void d(String msg) {
+        if (isLog()) {
+            Log.d(TAG, "d: " + msg);
+        }
+    }
+
+    static boolean isLog() {
         return BuildConfig.DEBUG;
     }
 }
