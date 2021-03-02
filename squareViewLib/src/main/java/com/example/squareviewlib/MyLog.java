2ce21852e2f22a6d58eb86ff3940945e68d03ba2
sts511 <33302991+su3233@users.noreply.github.com>
2021-03-02 15:39:57 +0800

    1\add MyLog

---
+++ squareViewLib/src/main/java/com/example/squareviewlib/MyLog.java
@@ -0,0 +1,15 @@
+package com.example.squareviewlib;
+
+/**
+ * @author: SuTs
+ * @created at: 2021/3/2 15:37
+ * @Describe:Log日志打印
+ */
+public class MyLog {
+
+
+
+    boolean isLog() {
+        return BuildConfig.DEBUG;
+    }
+}
