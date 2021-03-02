bea6db07b51004bd52fdfcdb6ba6e4b0c62f7fe7
sts511 <33302991+su3233@users.noreply.github.com>
2021-03-02 14:11:06 +0800

    1\commit

---
+++ squareViewLib/src/main/java/com/example/squareviewlib/SquareViewSu.java
@@ -0,0 +1,30 @@
+package com.example.squareviewlib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * @author: SuTs
+ * @created at: 2021/3/2 14:04
+ * @Describe:
+ */
+class SquareViewSu extends View {
+    public SquareViewSu(Context context) {
+        super(context);
+    }
+
+    public SquareViewSu(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareViewSu(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public SquareViewSu(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
